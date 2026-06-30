17111e433e0d5a6b91c5efa2ff4922016e9f4daa
Muhammad Bilal <bkbilalbluch@gmail.com>
2026-06-30 22:17:56 +0500

    Create main.cpp

---
+++ Cpp-for-DSA/Chapters/Chap 06_Input in C++/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // declare variable
+    int age ;
+    cout << "Enter your age is : ";
+    
+    // take input
+    cin >> age;
+    cout << "Your age is : " << age << endl;
+
+
+
+    return 0;
+}
