368abf529c1dc1619888e8c50354ff50af24bc1d
Muhammad Bilal <bkbilalbluch@gmail.com>
2026-07-01 10:25:00 +0500

    Update main.cpp

--- Cpp-for-DSA/Chapters/Chap 06_Input in C++/main.cpp
+++ Cpp-for-DSA/Chapters/Chap 06_Input in C++/main.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main(){
 
-    // declare variable
+    // declare variable for integers
     int age ;
-    cout << "Enter your age is : ";
+    cout << "Enter your age : ";
     
     // take input
     cin >> age;
@@ -14,4 +14,59 @@ int main(){
 
 
     return 0;
+
+
+    // declare variable for floating value 
+    float age ;
+    cout << "Enter your age : ";
+    
+    // take input
+    cin >> age;
+    cout << "Your age is : " << age << endl;
+
+
+
+    return 0;
+
+    
+    // declare variable for boolean
+    bool age ; // you can't use true / false in input operation you can use 1 or 0 
+    cout << "Enter your age : ";
+    
+    // take input
+    cin >> age;
+    cout << "Your age is : " << age << endl;
+
+
+
+    return 0;
+
+
+    // declare variable double 
+    double age ;
+    cout << "Enter your age : ";
+    
+    // take input
+    cin >> age;
+    cout << "Your age is : " << age << endl;
+
+
+
+    return 0;
+
+
+
+    // declare variable character value
+    char age ;
+    cout << "Enter your age : ";
+    
+    // take input
+    cin >> age;
+    cout << "Your age is : " << age << endl;
+
+
+
+    return 0;
+
+
 }
