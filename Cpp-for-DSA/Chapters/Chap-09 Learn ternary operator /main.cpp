26238bafdaa68ca48fdfa27240d7c9beeeb09c0b
Muhammad Bilal <bkbilalbluch@gmail.com>
2026-07-02 10:13:19 +0500

    Create main.cpp

---
+++ Cpp-for-DSA/Chapters/Chap-09 Learn ternary operator /main.cpp
@@ -0,0 +1,18 @@
+# include<iostream> 
+using namespace std;
+
+int main(){
+
+    int age = 55;
+    age > 18 ? cout <<"Can vote"<< endl :cout << "Can't vote" << endl;
+
+
+    int x = 10;
+    int y = 20;
+    
+    int result = (x > y) ? x : y;
+    cout << "Result : " << result << endl;
+
+
+    return 0;
+}
